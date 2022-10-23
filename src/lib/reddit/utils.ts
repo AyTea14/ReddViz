988908b02cdb380fd3989299ee9979690d08bb41
aytea14 <85985879+AyTea14@users.noreply.github.com>
2022-10-24 00:33:55 +0800

    updates

--- src/lib/reddit/utils.ts
+++ src/lib/reddit/utils.ts
@@ -15,10 +15,10 @@ export async function makeGetRequest(url: string, accessToken: string): Promise<
     const data = await request(url)
         .auth(accessToken, "Bearer")
         .options("throwOnError", true)
-        .headers("accept", "*/*")
-        .headers("host", "oauth.reddit.com")
-        .headers("user-agent", (process.env.USER_AGENT as string) ?? "MemeApi/0.0.1")
-        .headers("cache-control", "no-cache")
+        .header("accept", "*/*")
+        .header("host", "oauth.reddit.com")
+        .header("user-agent", (process.env.USER_AGENT as string) ?? "MemeApi/0.0.1")
+        .header("cache-control", "no-cache")
         .result()
         .then(async (data) => {
             return { body: (await data.body.json()) as Reddit.Response, statusCode: data.statusCode };
