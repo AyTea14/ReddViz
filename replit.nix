bbcd8285560808e90840a818deb681731a7a2c3f
AyTea14 <muhdamar4687@gmail.com>
2023-09-26 09:38:45 +0000

    replit

--- replit.nix
+++ replit.nix
@@ -1,10 +1,6 @@
 { pkgs }: {
     deps = [
-        pkgs.yarn
-        pkgs.esbuild
         pkgs.nodejs-16_x
-
-        pkgs.nodePackages.typescript
         pkgs.nodePackages.typescript-language-server
     ];
 }
